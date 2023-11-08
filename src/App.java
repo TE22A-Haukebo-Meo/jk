8981e1ec743546640503c7e623f09829f8f4e4b9
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-11-08 10:09:53 +0100

    uppg c klar

--- src/App.java
+++ src/App.java
@@ -1,9 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int tal=2;
-        while (tal<=5) {
-            System.out.println(tal);
-            tal=tal+1;
+        for (int i = 0; i < 6; i++){
+            System.out.println(i);
         }
+        }
+
     }
-}
+
