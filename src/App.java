f0b370b3a666a59ce000470adb92fab571ed62f0
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-11-08 10:34:58 +0100

    uppg g klar

--- src/App.java
+++ src/App.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        for(int i = 5; i >= 0; i--){
-            System.out.println(i);
+        System.out.println("Droppa 5 tal");
+        Scanner tb = new Scanner(System.in);
+        int[] tal = new int[5];
+        tal[0]=tb.nextInt();
+        tal[1]=tb.nextInt();
+        tal[2]=tb.nextInt();
+        tal[3]=tb.nextInt();
+        tal[4]=tb.nextInt();
+        for(int i=0; i<5;i++){
+            System.out.println("I fält "+i+" är talet "+tal[i]);
         }
+        
+        
     }
 }
